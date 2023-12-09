15dcbf310aede5491155230ec442bd315b224e43
forumwt <16545063+forumwt@users.noreply.github.com>
2023-12-09 11:24:47 +0100

    Update

--- user.js
+++ user.js
@@ -1,5 +1,5 @@
 // Style firefox
-user_pref("browser.startup.preXulSkeletonUI", false);
+//user_pref("browser.startup.preXulSkeletonUI", false);
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("svg.context-properties.content.enabled", true);
 user_pref("layout.css.color-mix.enabled", true);
